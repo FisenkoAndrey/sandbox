88d4187fdd4327352abbe60076eb7f2dfdc2359c
Andrei Fisenko <andrey.fisenko@yahoo.com>
2022-03-25 08:43:43 +0300

    Внесены изменения в PackagePoint.

--- src/main/java/PackagePoint/Point.java
+++ src/main/java/PackagePoint/Point.java
@@ -7,7 +7,7 @@ public class Point {
 
 
     public static double distance(Point p1, Point p2) {
-        double d = Math.sqrt((p1.x - p2.y) * (p1.x - p2.y) + (p1.x - p2.y) * (p1.x * p2.y));
+        double d = Math.sqrt((p1.x - p1.y) * (p1.x - p1.y) + (p2.x - p2.y) * (p2.x * p2.y));
         return d;
     }
 }
